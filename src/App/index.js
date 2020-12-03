1cc80832f87e93e1f4e8eb07aa8c48a9ab30fd8d
74ultra <r.barnhill@protonmail.com>
2020-12-02 17:59:18 -0800

    Add initial navigation bar structure

--- src/App/index.js
+++ src/App/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import WelcomeMessage from './WelcomeMessage.js';
 import styled, { css } from 'styled-components';
-import AppLayout from './AppLayout'
+import AppLayout from './AppLayout';
+import AppBar from './AppBar';
 
 
 function App() {
   return (
     <AppLayout>
+      <AppBar />
       <WelcomeMessage name={`CryptoDash`} />
     </AppLayout>
 
